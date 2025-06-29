7796dd5d3fd90d2b59b4e89d8922f4e9a5d92290
MaestroOG <muneeburrehman822@gmail.com>
2025-06-29 10:12:25 +0500

    fix responsiveness of hero section

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
   return (
     <BrowserRouter>
       <div className='relative z-0 bg-primary'>
-        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center mb-32'>
           <Navbar />
           <Hero />
         </div>
@@ -20,7 +20,7 @@ const App = () => {
           <Contact />
 
           <StarsCanvas />
-          <Footer/>
+          <Footer />
         </div>
       </div>
     </BrowserRouter>
